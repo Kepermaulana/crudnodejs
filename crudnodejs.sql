b1b16eee036bdce91772f00b1a7bb62055bed8a0
Rifqi Maulana <kepermaulana@gmail.com>
2022-11-26 18:03:33 +0700

    crudnodejs

---
+++ crudnodejs.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 23 Nov 2022 pada 13.44
+-- Versi server: 10.4.24-MariaDB
+-- Versi PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crudnodejs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `email`, `password`, `createdAt`, `updatedAt`) VALUES
+(1, 'rifqi', 'rifqi1@gmail.com', '123', '2022-11-11 13:55:44', '2022-11-11 13:55:44'),
+(2, 'kepermaulana', 'akukeper@gmail.com', '456', '2022-11-11 13:56:35', '2022-11-11 14:34:12'),
+(4, 'nopal', 'nopal1@gmail.com', '222', '2022-11-11 13:57:50', '2022-11-11 13:57:50'),
+(5, 'ghufron', 'ghufron@gmail.com', '233', '2022-11-11 13:58:28', '2022-11-11 13:58:28');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ models/User.js
@@ -0,0 +1,17 @@
+const { type } = require("express/lib/response");
+const Sequelize = require("sequelize");
+const db = require("../config/db");
+
+const User = db.define(
+    "user",
+    {
+        username: {type: Sequelize.STRING},
+        email: {type: Sequelize.STRING},
+        password: {type: Sequelize.STRING},
+    },
+    {
+        freezeTableName: true
+    }
+);
+
+module.exports = User;
